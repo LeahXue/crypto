e31b4fd233235012e011b773aab42fc1e6272de9
Leah Xue <leahxue@dynamic-oit-ip4-wifirestricted04-10-50-153-83.princeton.edu>
2024-11-12 18:48:57 -0500

    1v arb bot

--- optimizer.py
+++ optimizer.py
@@ -4,30 +4,20 @@ from .utils import get_crypto_prices, get_withdrawal_fees
 
 class Optimizer(Model): 
     def __init__(self, exchanges, **params): 
-        self.exchanges = exchanges 
-
-        # we allow arb w/ fiat currencies by default
-        # TODO: fix here 
-        self.include_fiat = True 
-        self.fiat_set = fiat 
-        self.token_set = tokens 
-
-    def init_currency_info(self): 
+        # init_currency_info 
         self.currency_set = set()
-        coin_set = set()
-        for exc_name, exchange in self.exchanges.items(): 
-            currency_names = ['{}_{}'.format(exc_name, cur) for cur in exchange.currencies.keys()]
-            coin_set |= set(exchange.currencies.keys())
-            self.currency_set |= set(currency_names) 
-            if not self.include_fiat: 
-                self.currency_set -= set(['{}_{}'.format(exc_name, fiat) for fiat in self.fiat_set])
-       
-        # we only consider coins that are stable enough (defined in info.py)
-        coin_set = coin_set & (self.token_set | self.fiat_set)
-        self.crypto_prices = get_crypto_prices(coin_set) 
-        self.currency_set = set([i for i in self.currency_set if i.split('_')[-1] in self.crypto_prices.keys()])
+        for 
 
-def find_arbitrage(): 
+    def update_price_mtx(df): 
+        transit_price_mtx = dict()
+        for i in range(len(df)): 
+            tiker = df["symbol"].iloc[i]
+            base = ticker.split("_")[0]
+            quote = ticker.split("_")[1]
+            if base in currency_set and quote in currency_set: 
+                transit_price_mtx[base, quote] = df["bid"].iloc[i]
+                transit_price_mtx[quote, base] = 1 / df["ask"].iloc[i]
+            
     
 
 
